7b2ea2d5986b352065a68cb888d59ed08622bd51
kevinsslin <kk123750964@gmail.com>
2023-07-19 10:46:42 +0800

    Add LopoGlobals and its interface

---
+++ contracts/LopoGlobals.sol
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import { ILopoGlobals } from "./interfaces/ILopoGlobals.sol";
+import {UD60x18, ud} from "@prb/math/UD60x18.sol";
+
+contract LopoGlobals is ILopoGlobals {
+
+    /*** Structs ***/
+
+    struct PoolDelegate {
+        address ownedPoolManager;
+        bool    isPoolDelegate;
+    }
+
+    /*** Storage ***/
+
+    address public override lopoVault;
+    address public override migrationAdmin;
+    address public override pendingLopoGovernor;
+
+    bool public override protocolPaused;
+
+    // configs share by all pools
+    UD60x18 public override riskFreeRate; 
+    UD60x18 public override minPoolLiquidityRatio;
+    UD60x18 public override protocolFeeRate;
+    
+    mapping(address => bool) public override isBorrower;
+    mapping(address => bool) public override isCollateralAsset;
+    mapping(address => bool) public override isPoolAsset;
+
+    // configs by poolManager
+    mapping(address => bool) public override isEnabled;
+    mapping(address => UD60x18) public override minDepositLimit;
+    mapping(address => uint256) public override withdrawalDurationInDays;
+    // mapping(address => address) public override insurancePool; // this should be implemented in other place
+    mapping(address => uint256) public override maxCoverLiquidationPercent;
+    mapping(address => uint256) public override minCoverAmount;
+    mapping(address => uint256) public override exitFeePercent;
+    mapping(address => uint256) public override insuranceFeePercent;
+    
+    mapping(address => PoolDelegate) public override poolDelegates;  
+
+    /*** Modifiers ***/
+
+    modifier onlyGovernor() {
+        _checkIsLopoGovernor();
+        _;
+    }
+
+    /*** Governor Transfer Functions ***/
+
+    function acceptLopoGovernor() external override {
+        require(msg.sender == pendingLopoGovernor, "LG:Caller_Not_Pending_Gov");
+        emit GovernorshipTransferred(admin(), msg.sender);
+        pendingLopoGovernor = address(0);
+        // lopoGovernor = msg.sender;
+        _setAddress(ADMIN_SLOT, msg.sender);
+    }
+
+    function setPendingLopoGovernor(address _pendingGovernor) external override onlyGovernor {
+        emit PendingGovernorSet(pendingLopoGovernor = _pendingGovernor);
+    }
+
+    /*** Global Setters ***/
+
+    // function activePoolManager(address _poolManager) external override onlyGovernor {
+    
+    // }
+
+    function setLopoVault(address _vault) external override onlyGovernor {
+        require(_vault != address(0), "LG:Invalid_Vault");
+        emit LopoVaultSet(lopoVault, _vault);
+        lopoVault = _vault;
+    }
+
+    // function setMigrationAdmin(address _migrationAdmin) external override onlyGovernor {
+    //     emit MigrationAdminSet(migrationAdmin, _migrationAdmin);
+    //     migrationAdmin = _migrationAdmin;
+    // }
+
+    /*** Boolean Setters ***/
+
+    function setProtocolPause(bool _protocolPaused) external override onlyGovernor {
+        emit ProtocolPauseSet(
+            msg.sender,
+            protocolPaused = _protocolPaused
+        );
+    }
+
+    /*** Allowlist Setters ***/
+
+    function setIsEnabled(address _poolManager, bool _isEnabled) external override onlyGovernor {
+        isEnabled[_poolManager] = _isEnabled;
+        emit IsEnabledSet(_poolManager, _isEnabled);
+    }
+
+    function setValidBorrower(address _borrower, bool _isValid) external override onlyGovernor {
+        isBorrower[_borrower] = _isValid;
+        emit ValidBorrowerSet(_borrower, _isValid);
+    }
+
+    function setValidCollateralAsset(address _collateralAsset, bool _isValid) external override onlyGovernor {
+        isCollateralAsset[_collateralAsset] = _isValid;
+        emit ValidCollateralAssetSet(_collateralAsset, _isValid);
+    }
+
+    function setValidPoolAsset(address _poolAsset, bool _isValid) external override onlyGovernor {
+        isPoolAsset[_poolAsset] = _isValid;
+        emit ValidPoolAssetSet(_poolAsset, _isValid);
+    }
+
+    function setValidPoolDelegate(address _account, bool _isValid) external override onlyGovernor {
+        require(_account != address(0), "LG:SVPD:ZERO_ADDR");
+
+        // Cannot remove pool delegates that own a pool manager.
+        require(_isValid || poolDelegates[_account].ownedPoolManager == address(0), "LG:SVPD:OWNS_POOL_MANAGER");
+
+        poolDelegates[_account].isPoolDelegate = _isValid;
+        emit ValidPoolDelegateSet(_account, _isValid);
+    }
+
+    /*** Cover Setters ***/
+
+    // function setMaxCoverLiquidationPercent(address _poolManager, uint256 _maxCoverLiquidationPercent) external override onlyGovernor {
+    //     require(_maxCoverLiquidationPercent <= HUNDRED_PERCENT, "LG:SMCLP:GT_100");
+    //     maxCoverLiquidationPercent[_poolManager] = _maxCoverLiquidationPercent;
+    //     emit MaxCoverLiquidationPercentSet(_poolManager, _maxCoverLiquidationPercent);
+    // }
+
+    // function setMinCoverAmount(address _poolManager, uint256 _minCoverAmount) external override onlyGovernor {
+    //     minCoverAmount[_poolManager] = _minCoverAmount;
+    //     emit MinCoverAmountSet(_poolManager, _minCoverAmount);
+    // }
+
+    
+
+    /*** Fee Setters ***/
+
+    function setRiskFreeRate(UD60x18 _riskFreeRate) external override onlyGovernor {
+        emit RiskFreeRateSet(riskFreeRate.intoUint256(), _riskFreeRate.intoUint256());
+        riskFreeRate = _riskFreeRate;
+    }
+
+    function setMinPoolLiquidityRatio(UD60x18 _minPoolLiquidityRatio) external override onlyGovernor {
+        emit MinPoolLiquidityRatioSet(minPoolLiquidityRatio.intoUint256(), _minPoolLiquidityRatio.intoUint256());
+        minPoolLiquidityRatio = _minPoolLiquidityRatio;
+    }
+
+    function setProtocolFeeRate(UD60x18 _protocolFeeRate) external override onlyGovernor {
+        emit ProtocolFeeRateSet(protocolFeeRate.intoUint256(), _protocolFeeRate.intoUint256());
+        protocolFeeRate = _protocolFeeRate;
+    }
+
+    /*** Pool Restriction Setters ***/
+
+    function setMinDepositLimit(address _poolManager, UD60x18 _minDepositLimit) external override onlyGovernor {
+        emit MinDepositLimitSet(_poolManager, minDepositLimit[_poolManager].intoUint256(), _minDepositLimit.intoUint256());
+        minDepositLimit[_poolManager] = _minDepositLimit;
+    }
+
+    function setWithdrawalDurationInDays(address _poolManager, uint256 _withdrawalDurationInDays) external override onlyGovernor {
+        emit WithdrawalDurationInDaysSet(_poolManager, withdrawalDurationInDays[_poolManager], _withdrawalDurationInDays);
+        withdrawalDurationInDays[_poolManager] = _withdrawalDurationInDays;
+    }
+
+    /*** View Function ***/
+
+    function governor() external view override returns (address governor_) {
+        governor_ = admin();
+    }
+
+    function isPoolDelegate(address account_) external view override returns (bool isPoolDelegate_) {
+        isPoolDelegate_ = poolDelegates[account_].isPoolDelegate;
+    }
+
+    function ownedPoolManager(address account_) external view override returns (address poolManager_) {
+        poolManager_ = poolDelegates[account_].ownedPoolManager;
+    }
+
+    /*** Helper Function ***/
+
+    function _checkIsLopoGovernor() internal view {
+        require(msg.sender == admin(), "LG:Caller_Not_Gov");
+    }
+
+    function _setAddress(bytes32 _slot, address _value) private {
+        assembly {
+            sstore(_slot, _value)
+        }
+    }
+
+}
---
+++ contracts/interfaces/ILopoGlobals.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import {UD60x18, ud} from "@prb/math/UD60x18.sol";
+
+interface ILopoGlobals {
+    /*** Events ***/
+
+    /**
+     *  @dev   The governorship has been accepted.
+     *  @param _previousGovernor The previous governor.
+     *  @param _currentGovernor  The new governor.
+     */
+    event GovernorshipTransferred(address indexed _previousGovernor, address indexed _currentGovernor);
+
+    /**
+     *  @dev   The pending governor has been set.
+     *  @param _pendingGovernor The new pending governor.
+     */
+    event PendingGovernorSet(address indexed _pendingGovernor);
+
+    /**
+     *  @dev   The address for the Maple treasury has been set.
+     *  @param _previousLopoVault The previous vault.
+     *  @param _currentLopoVault The new vault.
+     */
+    event LopoVaultSet(address indexed _previousLopoVault, address indexed _currentLopoVault);
+
+    /**
+     *  @dev   The migration admin has been set.
+     *  @param _previousMigrationAdmin The previous migration admin.
+     *  @param _currentMigrationAdmin The new migration admin.
+     */
+    event MigrationAdminSet(address indexed _previousMigrationAdmin, address indexed _currentMigrationAdmin);
+
+    /**
+     * @dev The risk free rate has been set.
+     * @param _previousRiskFreeRate The previous risk free rate.
+     * @param _currentRiskFreeRate The new risk free rate.
+     */
+    event RiskFreeRateSet(uint256 indexed _previousRiskFreeRate, uint256 indexed _currentRiskFreeRate);
+
+    /**
+     * @dev The minimum pool liquidity ratio has been set.
+     * @param _previousMinPoolLiquidityRatio The previous minimum pool liquidity ratio.
+     * @param _currentMinPoolLiquidityRatio The new minimum pool liquidity ratio.
+     */
+    event MinPoolLiquidityRatioSet(uint256 indexed _previousMinPoolLiquidityRatio, uint256 indexed _currentMinPoolLiquidityRatio);
+
+    /**
+     * @dev The protocol fee rate has been set.
+     * @param _previousProtocolFeeRate The previous protocol fee rate.
+     * @param _currentProtocolFeeRate The new protocol fee rate.
+     */
+    event ProtocolFeeRateSet(uint256 indexed _previousProtocolFeeRate, uint256 indexed _currentProtocolFeeRate);
+
+    /**
+     *  @dev   The protocol pause was set to a new state.
+     *  @param _caller         The address of the security admin or governor that performed the action.
+     *  @param _protocolPaused The protocol paused state.
+     */
+    event ProtocolPauseSet(address indexed _caller, bool _protocolPaused);
+
+    /**
+     * @dev The pool is enabled or disabled.
+     * @param _poolManager The address of the pool manager.
+     * @param _isEnabled The enabled state of the pool.
+     */
+    event IsEnabledSet(address indexed _poolManager, bool _isEnabled);
+
+
+    /**
+     *  @dev   A valid borrower was set.
+     *  @param _borrower The address of the borrower.
+     *  @param _isValid  The validity of the borrower.
+     */
+    event ValidBorrowerSet(address indexed _borrower, bool _isValid);
+
+    /**
+     *  @dev   A valid asset was set.
+     *  @param _collateralAsset The address of the collateral asset.
+     *  @param _isValid         The validity of the collateral asset.
+     */
+    event ValidCollateralAssetSet(address indexed _collateralAsset, bool _isValid);
+
+    /**
+     *  @dev   A valid asset was set.
+     *  @param _poolAsset The address of the asset.
+     *  @param _isValid   The validity of the asset.
+     */
+    event ValidPoolAssetSet(address indexed _poolAsset, bool _isValid);
+
+    /**
+     *  @dev   A valid pool delegate was set.
+     *  @param _account The address the account.
+     *  @param _isValid The validity of the asset.
+     */
+    event ValidPoolDelegateSet(address indexed _account, bool _isValid);
+
+
+    /*** View Function ***/
+    function lopoVault() external view returns (address _lopoVault);
+
+    // function governor() external view returns (address _governor);
+
+    function migrationAdmin() external view returns (address _migrationAdmin);
+
+    function pendingLopoGovernor() external view returns (address _pendingLopoGovernor);
+
+    function protocolPaused() external view returns (bool _protocolPaused);
+
+    function isBorrower(address _borrower) external view returns (bool _isBorrower);
+
+    function isCollateralAsset(address _asset) external view returns (bool _isCollateralAsset);
+
+    function isPoolAsset(address _asset) external view returns (bool _isPoolAsset);
+
+    // function isPoolDelegate(address _delegate) external view returns (bool _isPoolDelegate);
+    
+    function poolDelegates(address _poolDelegate) external view returns (address ownedPoolManager, bool isPoolDelegate);
+
+    function isContractPaused(address _contract) external view returns (bool _isContractPaused);
+
+    //  function isFactory(bytes32 _factoryId, address _factory) external view returns (bool _isFactory);
+
+
+
+}
