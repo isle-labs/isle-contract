aa7238e2ae39043b08cfa2cefa8ac06e02cf108e
kevinsslin <kk123750964@gmail.com>
2023-08-10 03:08:13 +0800

    add some unit test for Pool.sol

--- tests/PoolUnit.t.sol
+++ tests/PoolUnit.t.sol
@@ -55,6 +55,96 @@ contract PoolUnitTest is PoolBase {
         assertAlmostEq(newAllowance, 0, _delta_);
     }
 
+    function test_mint() public {
+        vm.prank(caller);
+        asset.approve(address(pool), 1000);
+
+        uint256 oldCallerAsset = asset.balanceOf(caller);
+        uint256 oldReceiverShare = pool.balanceOf(receiver);
+        uint256 oldAllowance = asset.allowance(caller, address(pool));
+
+        vm.prank(caller);
+        pool.mint(1000, receiver);
+
+        uint256 newCallerAsset = asset.balanceOf(caller);
+        uint256 newReceiverShare = pool.balanceOf(receiver);
+        uint256 newAllowance = asset.allowance(caller, address(pool));
+
+        assertAlmostEq(newCallerAsset, oldCallerAsset - 1000, _delta_);
+        assertAlmostEq(newReceiverShare, oldReceiverShare + 1000, _delta_);
+
+        if (oldAllowance != type(uint256).max) {
+            assertAlmostEq(newAllowance, oldAllowance - 1000, _delta_, "allowance");
+        }
+    }
+
+    function test_mintWithPermit() public {
+        uint256 oldCallerAsset = asset.balanceOf(caller);
+        uint256 oldReceiverShare = pool.balanceOf(receiver);
+
+        uint256 callerPrivateKey = PRIVATE_KEYS[8];
+        (uint8 v, bytes32 r, bytes32 s) = _getValidPermitSignature(
+            address(asset), caller, address(pool), 1000, 0, block.timestamp + 1 days, callerPrivateKey
+        );
+        
+        vm.prank(caller);
+        pool.mintWithPermit(1000, receiver, 1000, block.timestamp + 1 days, v, r, s);
+
+        uint256 newCallerAsset = asset.balanceOf(caller);
+        uint256 newReceiverShare = pool.balanceOf(receiver);
+        uint256 newAllowance = asset.allowance(caller, address(pool));
+
+        assertAlmostEq(newCallerAsset, oldCallerAsset - 1000, _delta_);
+        assertAlmostEq(newReceiverShare, oldReceiverShare + 1000, _delta_);
+        assertAlmostEq(newAllowance, 0, _delta_);
+    }
+    
+    //TODO: add a test that caller of withdraw() is not the owner of the assets
+    function test_withdraw() public {
+        vm.startPrank(caller);
+        asset.approve(address(pool), 1000);
+        pool.deposit(1000, receiver);
+        vm.stopPrank();
+
+        uint256 oldCallerAsset = asset.balanceOf(caller);
+        uint256 oldReceiverShare = pool.balanceOf(receiver);
+        uint256 oldAllowance = asset.allowance(caller, address(pool));
+
+        // notice that we are withdrawing assets from receiver, not caller
+        vm.prank(receiver);
+        uint256 assets = pool.withdraw(1000, caller, receiver);
+
+        uint256 newCallerAsset = asset.balanceOf(caller);
+        uint256 newReceiverShare = pool.balanceOf(receiver);
+        uint256 newAllowance = asset.allowance(caller, address(pool));
+
+        assertAlmostEq(newCallerAsset, oldCallerAsset + assets, _delta_);
+        assertAlmostEq(newReceiverShare, oldReceiverShare - assets, _delta_);
+    }
+
+    // TODO: add a test that caller of redeem() is not the owner of the shares
+    function test_redeem() public {
+        vm.startPrank(caller);
+        asset.approve(address(pool), 1000);
+        uint256 shares = pool.deposit(1000, receiver);
+        vm.stopPrank();
+
+        uint256 oldCallerAsset = asset.balanceOf(caller);
+        uint256 oldReceiverShare = pool.balanceOf(receiver);
+        uint256 oldAllowance = pool.allowance(caller, address(pool));
+
+        // notice that we are redeeming shares from receiver
+        vm.prank(receiver);
+        uint256 assets = pool.redeem(shares, caller, receiver);
+
+        uint256 newCallerAsset = asset.balanceOf(caller);
+        uint256 newReceiverShare = pool.balanceOf(receiver);
+        uint256 newAllowance = pool.allowance(caller, address(pool));
+
+        assertAlmostEq(newCallerAsset, oldCallerAsset + assets, _delta_);
+        assertAlmostEq(newReceiverShare, oldReceiverShare - assets, _delta_);
+    }
+
     /* ========== Helper Functions ========== */
 
     // Returns a valid `permit` signature signed by this contract's `owner` address
--- tests/mocks/MockPoolConfigurator.sol
+++ tests/mocks/MockPoolConfigurator.sol
@@ -6,7 +6,27 @@ import "../../contracts/PoolConfigurator.sol";
 contract MockPoolConfigurator is PoolConfigurator {
     constructor(IPoolAddressesProvider provider_) PoolConfigurator(provider_) { }
 
-    function maxDeposit(address receiver_) external view override returns (uint256) {
+    function maxDeposit(address receiver_) external pure override returns (uint256) {
         return type(uint256).max;
+    }
+
+    function maxMint(address receiver_) external pure override returns (uint256) {
+        return type(uint256).max;
+    }
+
+    function maxWithdraw(address receiver_) external pure override returns (uint256) {
+        return type(uint256).max;
+    }
+
+    function maxRedeem(address receiver_) external pure override returns (uint256) {
+        return type(uint256).max;
+    }
+
+    function previewWithdraw(address owner_, uint256 assets_) external pure override returns (uint256) {
+        return assets_;
+    }
+
+    function previewRedeem(address receiver_, uint256 shares_) external pure override returns (uint256) {
+        return shares_;
     }
 }
