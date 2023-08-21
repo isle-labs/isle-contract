7e342375f811c0ac69ec702bae2e2cc07794bfef
kevinsslin <kk123750964@gmail.com>
2023-08-21 17:13:27 +0800

    move Pool test to Integration

--- tests/integration/PoolHarness.t.sol
+++ tests/integration/PoolHarness.t.sol
@@ -1,16 +1,98 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity 0.8.19;
 
-import "./PoolBaseUint.t.sol";
-import { UD60x18, ud } from "@prb/math/UD60x18.sol";
+import "./Integration.t.sol";
+import { Pool } from "../../contracts/Pool.sol";
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 
-contract PoolHarnessTest is PoolBaseUint {
-    PoolHarness poolHarness;
+// Notice that this contract is for testing internal functions of Pool contract
+contract PoolHarness is Pool {
+    constructor(
+        address poolConfigurator_,
+        address asset_,
+        string memory lpTokenName_,
+        string memory lpTokenSymbol_
+    )
+        Pool(poolConfigurator_, asset_, lpTokenName_, lpTokenSymbol_)
+    { }
+
+    function exposed_convertToShares(
+        uint256 assets_,
+        Math.Rounding rounding_
+    )
+        external
+        view
+        returns (uint256 shares_)
+    {
+        return _convertToShares(assets_, rounding_);
+    }
+
+    function exposed_convertToExitShares(
+        uint256 assets_,
+        Math.Rounding rounding_
+    )
+        external
+        view
+        returns (uint256 shares_)
+    {
+        return _convertToExitShares(assets_, rounding_);
+    }
+
+    function exposed_convertToAssets(
+        uint256 shares_,
+        Math.Rounding rounding_
+    )
+        external
+        view
+        returns (uint256 assets_)
+    {
+        return _convertToAssets(shares_, rounding_);
+    }
+
+    function exposed_convertToExitAssets(
+        uint256 shares_,
+        Math.Rounding rounding_
+    )
+        external
+        view
+        returns (uint256 assets_)
+    {
+        return _convertToExitAssets(shares_, rounding_);
+    }
+
+    function exposed_deposit(address caller, address receiver, uint256 assets, uint256 shares) external {
+        _deposit(caller, receiver, assets, shares);
+    }
+
+    function exposed_withdraw(
+        address caller,
+        address receiver,
+        address owner,
+        uint256 assets,
+        uint256 shares
+    )
+        external
+    {
+        _withdraw(caller, receiver, owner, assets, shares);
+    }
+
+    function exposed_decimalsOffset() external pure returns (uint256) {
+        return _decimalsOffset();
+    }
+}
+
+contract PoolHarnessTest is IntegrationTest {
+    PoolHarness internal poolHarness;
     uint256 internal _delta_ = 1e6; // in USDC's case
 
     function setUp() public override {
         super.setUp();
-        poolHarness = new PoolHarness(address(mockPoolConfigurator), address(usdc), "lpToken", "LPT");
+        poolHarness = new PoolHarness(address(wrappedPoolConfiguratorProxy), address(usdc), "lpToken", "LPT");
+
+        vm.startPrank(users.pool_admin);
+        wrappedPoolConfiguratorProxy.setLiquidityCap(1_000_000e6);
+        wrappedPoolConfiguratorProxy.setValidLender(address(this), true);
+        vm.stopPrank();
     }
 
     function test_exposed_convertToShares() public {
@@ -41,7 +123,7 @@ contract PoolHarnessTest is PoolBaseUint {
         // let the unrealizedLosses = 5000e6
         // shares = 1000e6 * (1_000_000e6 + 1) / (1_050_000e6 - 5000e6 + 1)
         uint256 shares = poolHarness.exposed_convertToExitShares(1000e6, Math.Rounding.Down);
-        UD60x18 result = ud(1000e6).mul(ud(1_000_000e6 + 1)).div(ud(1_050_000e6 - 5000e6 + 1));
+        UD60x18 result = ud(1000e6).mul(ud(1_000_000e6 + 1)).div(ud(1_050_000e6 - 0e6 + 1));
 
         assertAlmostEq(shares, result.intoUint256(), _delta_);
     }
@@ -74,7 +156,7 @@ contract PoolHarnessTest is PoolBaseUint {
         // let the unrealizedLosses = 5000e6
         // shares = 1000e6 * (1_050_000e6 - 5000e6 + 1) / (1_000_000e6 + 1)
         uint256 assets = poolHarness.exposed_convertToExitAssets(1000e6, Math.Rounding.Down);
-        UD60x18 result = ud(1000e6).mul(ud(1_050_000e6 - 5000e6 + 1)).div(ud(1_000_000e6 + 1));
+        UD60x18 result = ud(1000e6).mul(ud(1_050_000e6 - 0e6 + 1)).div(ud(1_000_000e6 + 1));
 
         assertAlmostEq(assets, result.intoUint256(), _delta_);
     }
@@ -96,26 +178,6 @@ contract PoolHarnessTest is PoolBaseUint {
         assertAlmostEq(newThisAsset, oldThisAsset - 500e6, _delta_, "thisAsset");
         assertAlmostEq(newThisShare, oldThisShare + 2000e6, _delta_, "thisShare");
         assertAlmostEq(newAllowance, oldAllowance - 500e6, _delta_, "allowance");
-    }
-
-    function test_exposed_withdraw() public {
-        usdc.mint(address(this), 1000e6);
-        usdc.approve(address(poolHarness), 1000e6);
-        poolHarness.deposit(1000e6, address(this));
-
-        uint256 oldThisAsset = usdc.balanceOf(address(this));
-        uint256 oldThisShare = poolHarness.balanceOf(address(this));
-        uint256 oldAllowance = usdc.allowance(address(this), address(poolHarness));
-
-        poolHarness.exposed_withdraw(address(this), address(this), address(this), 700e6, 1000e6);
-
-        uint256 newThisAsset = usdc.balanceOf(address(this));
-        uint256 newThisShare = poolHarness.balanceOf(address(this));
-        uint256 newAllowance = usdc.allowance(address(this), address(poolHarness));
-
-        assertAlmostEq(newThisAsset, oldThisAsset + 700e6, _delta_, "thisAsset");
-        assertAlmostEq(newThisShare, oldThisShare - 1000e6, _delta_, "thisShare");
-        assertAlmostEq(newAllowance, oldAllowance, _delta_, "allowance");
     }
 
     function test_exposed_decimalsOffset() public {
