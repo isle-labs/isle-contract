9b5f8c4f13b45e96cc45d27a5e2e1b242a5e3451
Terrance Yu-Hao Chen <b08902073@csie.ntu.edu.tw>
2023-09-05 09:55:24 +0000

    Remove console2

--- contracts/WithdrawalManager.sol
+++ contracts/WithdrawalManager.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import { console2 } from "@forge-std/console2.sol";
-
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import { IERC20, SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
