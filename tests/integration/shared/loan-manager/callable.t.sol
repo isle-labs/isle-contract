dec0ccba457d72b36f5ec1986ba19c5409a75551
kevinsslin <kk123750964@gmail.com>
2023-09-18 15:35:48 +0800

    add callable shared file

--- tests/integration/shared/loan-manager/callable.t.sol
+++ tests/integration/shared/loan-manager/callable.t.sol
@@ -3,8 +3,16 @@ pragma solidity ^0.8.19;
 
 import { LoanManager_Integration_Shared_Test } from "./loanManager.t.sol";
 
-abstract contract Loan_Integration_Shared_Test is LoanManager_Integration_Shared_Test {
+abstract contract Callable_Integration_Shared_Test is LoanManager_Integration_Shared_Test {
     function setUp() public virtual override {
         changePrank(users.poolAdmin);
+    }
+
+    modifier WhenNotPaused() {
+        _;
+    }
+
+    modifier WhenCallerPoolAdminOrGovernor() {
+        _;
     }
 }
