934e872e4ab734a3e474b87e4577c13c6dc66d66
kevinsslin <kk123750964@gmail.com>
2023-09-28 13:04:28 +0800

    correct LoanManager.t.sol

--- tests/integration/concrete/loan-manager/LoanManager.t.sol
+++ tests/integration/concrete/loan-manager/LoanManager.t.sol
@@ -7,28 +7,14 @@ abstract contract LoanManager_Integration_Concrete_Test is Integration_Test {
     function setUp() public virtual override(Integration_Test) {
         Integration_Test.setUp();
 
-        changePrank(users.poolAdmin);
-        setUpPoolConfigurator();
-
-        changePrank(users.caller);
-        setUpPool(); // initialized pool state for testing
+        initializePool();
 
         // Make the msg sender the default pool admin
         changePrank(users.poolAdmin);
+        depositCover();
     }
 
-    function setUpPoolConfigurator() internal {
-        poolConfigurator.setOpenToPublic(true);
-        poolConfigurator.setLiquidityCap(defaults.POOL_LIMIT());
-        poolConfigurator.setValidLender(users.receiver, true);
-        poolConfigurator.setValidLender(users.caller, true);
-
-        poolConfigurator.setValidBuyer(users.buyer, true);
-        poolConfigurator.setValidSeller(users.seller, true);
-    }
-
-    function setUpPool() internal {
-        // Caller is the singler depositor initially
-        pool.deposit({ assets: defaults.POOL_LIQUIDITY(), receiver: users.caller });
+    function depositCover() internal {
+        poolConfigurator.depositCover(defaults.COVER_AMOUNT());
     }
 }
