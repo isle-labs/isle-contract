a9bde5f4d73d1e3791b4cdd1cd511cbcfce38532
kevinsslin <kk123750964@gmail.com>
2023-10-03 15:49:30 +0800

    run forge fmt

--- tests/integration/shared/lopo-globals/callable.t.sol
+++ tests/integration/shared/lopo-globals/callable.t.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.19;
 import { LopoGlobals_Integration_Shared_Test } from "./LopoGlobals.t.sol";
 
 abstract contract Callable_Integration_Shared_Test is LopoGlobals_Integration_Shared_Test {
-    function setUp() public virtual override {
-    }
+    function setUp() public virtual override { }
 
     modifier WhenCallerGovernor() {
         changePrank(users.governor);
