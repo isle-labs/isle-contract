b564845dbf61130f18bee7109ca2fdc6799fdb09
kevinsslin <kk123750964@gmail.com>
2023-10-16 16:12:06 +0800

    fix DeployGlobals script

--- scripts/DeployGlobals.s.sol
+++ scripts/DeployGlobals.s.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
+import { UUPSProxy } from "../contracts/libraries/upgradability/UUPSProxy.sol";
+
 import { IsleGlobals } from "../contracts/IsleGlobals.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
 contract DeployGlobals is BaseScript {
     function run() public virtual broadcast(deployer) returns (IsleGlobals globals_) {
-        globals_ = new IsleGlobals();
+        globals_ = IsleGlobals(address(new UUPSProxy(address(new IsleGlobals()), "")));
         globals_.initialize(governor);
     }
 }
