5cad3c6d9420864592220b7ac81a237c312dfe14
Terrance Chen <terrance@bsos.co>
2023-11-14 08:38:50 +0000

    Add github workflow to update accounting of pool

---
+++ scripts/updateAccounting.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19;
+
+import "@forge-std/console2.sol";
+
+import { ILoanManager } from "../contracts/interfaces/ILoanManager.sol";
+import { IPoolAddressesProvider } from "../contracts/interfaces/IPoolAddressesProvider.sol";
+
+import { Loan } from "../contracts/libraries/types/DataTypes.sol";
+
+import { BaseScript } from "./Base.s.sol";
+
+contract repayLoan is BaseScript {
+    function run(IPoolAddressesProvider poolAddressesProvider_) public broadcast(governor) {
+        ILoanManager loanManager_ = ILoanManager(poolAddressesProvider_.getLoanManager());
+        loanManager_.updateAccounting();
+    }
+}
