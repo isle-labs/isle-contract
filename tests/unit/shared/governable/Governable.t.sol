1ae3ceea5925eed263409d4304156fd28ac36f09
Jung <44439767+Jung0x12@users.noreply.github.com>
2024-07-30 00:37:27 +0800

    test: Add test case transfer governor for Governable (#43)
    * Add test transfer governor for Governable
    
    * move MockGovernable to mock folder
    
    * chore: change the import layout of Governable.t.sol
    
    ---------
    
    Co-authored-by: @kevinsslin

---
+++ tests/unit/shared/governable/Governable.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19;
+
+import { Base_Test } from "../../../Base.t.sol";
+
+import { MockGovernable } from "../../../mocks/MockGovernable.sol";
+
+abstract contract Governable_Test is Base_Test {
+    MockGovernable public mockGovernable;
+
+    function setUp() public virtual override {
+        Base_Test.setUp();
+        mockGovernable = new MockGovernable(users.governor);
+    }
+
+    modifier whenCallerGovernor() {
+        changePrank(users.governor);
+        _;
+    }
+}
