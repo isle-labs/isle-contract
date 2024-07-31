c226724aa04d5b42488f27a2a6e4cd3430500f1c
Jung <jwork1994212@gmail.com>
2024-07-31 16:24:43 +0800

    processExit test case supplement

--- tests/integration/concrete/withdrawal-manager/process-exit/processExit.t.sol
+++ tests/integration/concrete/withdrawal-manager/process-exit/processExit.t.sol
@@ -1,14 +1,65 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import { Errors } from "contracts/libraries/Errors.sol";
+
 import { WithdrawalManager_Integration_Shared_Test } from "../../../shared/withdrawal-manager/WithdrawalManager.t.sol";
 
 contract processExit_Integration_Concrete_Test is WithdrawalManager_Integration_Shared_Test {
+    modifier hasRequest() {
+        _;
+    }
+
+    modifier validRequestShares() {
+        _;
+    }
+
+    modifier inTheWindow() {
+        _;
+    }
+
     function setUp() public virtual override(WithdrawalManager_Integration_Shared_Test) {
         WithdrawalManager_Integration_Shared_Test.setUp();
     }
 
-    function test_processExit() public whenCallerPoolConfigurator {
+    function test_RevertWhen_NotPoolConfigurator() external {
+        changePrank(users.receiver);
+        vm.expectRevert(abi.encodeWithSelector(Errors.NotPoolConfigurator.selector, users.receiver));
+        withdrawalManager.processExit({ requestedShares_: 0, owner_: users.receiver });
+    }
+
+    function test_RevertWhen_NoRequest() external whenCallerPoolConfigurator {
+        vm.expectRevert(abi.encodeWithSelector(Errors.WithdrawalManager_NoRequest.selector, users.receiver));
+        withdrawalManager.processExit({ requestedShares_: 0, owner_: users.receiver });
+    }
+
+    function test_RevertWhen_InvalidShares() external whenCallerPoolConfigurator hasRequest {
+        addDefaultShares();
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Errors.WithdrawalManager_InvalidShares.selector, users.receiver, 0, defaults.ADD_SHARES()
+            )
+        );
+        withdrawalManager.processExit({ requestedShares_: 0, owner_: users.receiver });
+    }
+
+    function test_RevertWhen_NotInWindow() external whenCallerPoolConfigurator hasRequest validRequestShares {
+        uint256 addShares_ = defaults.ADD_SHARES();
+        uint64 startWindow = defaults.WINDOW_3();
+        uint64 endWindow = defaults.WINDOW_3() + defaults.WINDOW_DURATION();
+
+        addDefaultShares();
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Errors.WithdrawalManager_NotInWindow.selector, block.timestamp, startWindow, endWindow
+            )
+        );
+        withdrawalManager.processExit({ requestedShares_: addShares_, owner_: users.receiver });
+    }
+
+    function test_processExit() public whenCallerPoolConfigurator hasRequest validRequestShares inTheWindow {
         uint256 addShares_ = defaults.ADD_SHARES();
 
         uint256 expectedRedeemableShares_ = addShares_;
@@ -30,6 +81,9 @@ contract processExit_Integration_Concrete_Test is WithdrawalManager_Integration_
 
         (uint256 actualRedeemableShares_, uint256 actualResultingAssets_) =
             withdrawalManager.processExit({ requestedShares_: addShares_, owner_: users.receiver });
+
+        assertEq(withdrawalManager.exitCycleId(users.receiver), 0);
+        assertEq(withdrawalManager.lockedShares(users.receiver), 0);
 
         assertEq(actualRedeemableShares_, expectedRedeemableShares_);
         assertEq(actualResultingAssets_, expectedResultingAssets_);
