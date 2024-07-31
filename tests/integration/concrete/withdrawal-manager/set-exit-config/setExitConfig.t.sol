86a8a4a40c470b5be602084cbd5d7c8ac2948b68
Jung <jwork1994212@gmail.com>
2024-07-31 17:16:10 +0800

    setExitConfig test case supplement

--- tests/integration/concrete/withdrawal-manager/set-exit-config/setExitConfig.t.sol
+++ tests/integration/concrete/withdrawal-manager/set-exit-config/setExitConfig.t.sol
@@ -1,16 +1,69 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import { Errors } from "contracts/libraries/Errors.sol";
+
 import { WithdrawalManager } from "contracts/libraries/types/DataTypes.sol";
 
 import { WithdrawalManager_Integration_Shared_Test } from "../../../shared/withdrawal-manager/WithdrawalManager.t.sol";
 
 contract setExitConfig_Integration_Concrete_Test is WithdrawalManager_Integration_Shared_Test {
+    modifier whenProtocolNotPaused() {
+        _;
+    }
+
+    modifier whenNewWindowlNotZero() {
+        _;
+    }
+
+    modifier whenNewWindowlNotGreaterThanNewCycle() {
+        _;
+    }
+
     function setUp() public virtual override(WithdrawalManager_Integration_Shared_Test) {
         WithdrawalManager_Integration_Shared_Test.setUp();
     }
 
-    function test_setExitConfig() public whenCallerPoolAdmin {
+    function test_RevertWhen_ProtocolPaused() external {
+        changePrank(users.governor);
+        isleGlobals.setProtocolPaused(true);
+        vm.expectRevert(abi.encodeWithSelector(Errors.ProtocolPaused.selector));
+        setDefaultNewExitConfig();
+    }
+
+    function test_RevertWhen_NotPoolAdmin() external whenProtocolNotPaused {
+        changePrank(users.caller);
+        vm.expectRevert(abi.encodeWithSelector(Errors.NotPoolAdmin.selector, users.caller));
+        setDefaultNewExitConfig();
+    }
+
+    function test_RevertWhen_ZeroWindow() external whenProtocolNotPaused whenCallerPoolAdmin {
+        uint256 newCycleDuration_ = defaults.NEW_CYCLE_DURATION();
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.WithdrawalManager_ZeroWindow.selector));
+        withdrawalManager.setExitConfig({ cycleDuration_: newCycleDuration_, windowDuration_: 0 });
+    }
+
+    function test_RevertWhen_WindowGreaterThanCycle()
+        external
+        whenProtocolNotPaused
+        whenCallerPoolAdmin
+        whenNewWindowlNotZero
+    {
+        uint256 newCycleDuration_ = defaults.NEW_CYCLE_DURATION();
+        uint256 newWindowDuration_ = defaults.NEW_WINDOW_DURATION() + 15 days;
+
+        vm.expectRevert(abi.encodeWithSelector(Errors.WithdrawalManager_WindowGreaterThanCycle.selector));
+        withdrawalManager.setExitConfig({ cycleDuration_: newCycleDuration_, windowDuration_: newWindowDuration_ });
+    }
+
+    function test_setExitConfig()
+        public
+        whenProtocolNotPaused
+        whenCallerPoolAdmin
+        whenNewWindowlNotZero
+        whenNewWindowlNotGreaterThanNewCycle
+    {
         WithdrawalManager.CycleConfig memory expectedLatestConfig_ = WithdrawalManager.CycleConfig({
             initialCycleId: 4,
             initialCycleTime: defaults.WINDOW_4(),
