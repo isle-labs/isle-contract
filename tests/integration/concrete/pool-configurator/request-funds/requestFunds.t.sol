2bed82e892a33b3e61669222f36653af7f98c7ca
Jung <jwork1994212@gmail.com>
2024-08-01 15:42:46 +0800

    add pool supply insufficient case for requestFund

--- tests/integration/concrete/pool-configurator/request-funds/requestFunds.t.sol
+++ tests/integration/concrete/pool-configurator/request-funds/requestFunds.t.sol
@@ -8,6 +8,16 @@ import { PoolConfigurator_Integration_Shared_Test } from "../../../shared/pool-c
 contract RequestFunds_Integration_Concrete_Test is PoolConfigurator_Integration_Shared_Test {
     uint256 private _principal;
 
+    modifier whenCoverIsSufficient() {
+        changePrank(users.poolAdmin);
+        poolConfigurator.depositCover(defaults.COVER_AMOUNT());
+        _;
+    }
+
+    modifier whenPoolSupplyIsSufficient() {
+        _;
+    }
+
     function setUp() public virtual override(PoolConfigurator_Integration_Shared_Test) {
         PoolConfigurator_Integration_Shared_Test.setUp();
 
@@ -39,12 +49,35 @@ contract RequestFunds_Integration_Concrete_Test is PoolConfigurator_Integration_
         poolConfigurator.requestFunds({ principal_: _principal });
     }
 
-    function test_RevertWhen_InsufficientCover() external whenFunctionNotPause whenCallerLoanManager {
+    function test_RevertWhen_PoolSupplyZero() external whenFunctionNotPause {
+        // drain the pool
+        changePrank(users.receiver);
+        pool.requestRedeem(defaults.POOL_SHARES(), users.receiver);
+        vm.warp(defaults.WINDOW_3());
+        pool.redeem(defaults.POOL_SHARES(), users.receiver, users.receiver);
+
+        changePrank({ msgSender: address(loanManager) });
+        vm.expectRevert(abi.encodeWithSelector(Errors.PoolConfigurator_PoolSupplyZero.selector));
+        poolConfigurator.requestFunds({ principal_: _principal });
+    }
+
+    function test_RevertWhen_InsufficientCover()
+        external
+        whenFunctionNotPause
+        whenCallerLoanManager
+        whenPoolSupplyIsSufficient
+    {
         vm.expectRevert(abi.encodeWithSelector(Errors.PoolConfigurator_InsufficientCover.selector));
         poolConfigurator.requestFunds({ principal_: _principal });
     }
 
-    function test_requestFunds() external whenCoverIsSufficient whenFunctionNotPause whenCallerLoanManager {
+    function test_requestFunds()
+        external
+        whenCoverIsSufficient
+        whenFunctionNotPause
+        whenCallerLoanManager
+        whenPoolSupplyIsSufficient
+    {
         expectCallToTransferFrom({ from: address(pool), to: address(loanManager), amount: _principal });
         poolConfigurator.requestFunds({ principal_: _principal });
     }
@@ -52,11 +85,5 @@ contract RequestFunds_Integration_Concrete_Test is PoolConfigurator_Integration_
     function expectPoolConfiguratorPauseRevert() private {
         vm.expectRevert(abi.encodeWithSelector(Errors.PoolConfigurator_Paused.selector));
         poolConfigurator.requestFunds({ principal_: _principal });
-    }
-
-    modifier whenCoverIsSufficient() {
-        changePrank(users.poolAdmin);
-        poolConfigurator.depositCover(defaults.COVER_AMOUNT());
-        _;
     }
 }
