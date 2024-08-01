9cd384e5e7f4e483d230eb1bd8f7afc314722845
Jung <jwork1994212@gmail.com>
2024-08-01 16:56:12 +0800

    add insufficient liquidate case for requestFund

--- tests/integration/concrete/pool-configurator/request-funds/requestFunds.t.sol
+++ tests/integration/concrete/pool-configurator/request-funds/requestFunds.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
 import { Errors } from "contracts/libraries/Errors.sol";
 
 import { PoolConfigurator_Integration_Shared_Test } from "../../../shared/pool-configurator/PoolConfigurator.t.sol";
@@ -50,11 +52,7 @@ contract RequestFunds_Integration_Concrete_Test is PoolConfigurator_Integration_
     }
 
     function test_RevertWhen_PoolSupplyZero() external whenFunctionNotPause {
-        // drain the pool
-        changePrank(users.receiver);
-        pool.requestRedeem(defaults.POOL_SHARES(), users.receiver);
-        vm.warp(defaults.WINDOW_3());
-        pool.redeem(defaults.POOL_SHARES(), users.receiver, users.receiver);
+        _drainThePool();
 
         changePrank({ msgSender: address(loanManager) });
         vm.expectRevert(abi.encodeWithSelector(Errors.PoolConfigurator_PoolSupplyZero.selector));
@@ -71,6 +69,19 @@ contract RequestFunds_Integration_Concrete_Test is PoolConfigurator_Integration_
         poolConfigurator.requestFunds({ principal_: _principal });
     }
 
+    function test_RevertWhen_InsufficientLiquidity()
+        external
+        whenFunctionNotPause
+        whenCoverIsSufficient
+        whenPoolSupplyIsSufficient
+    {
+        _createInsufficientLiquidityPool();
+
+        changePrank({ msgSender: address(loanManager) });
+        vm.expectRevert(abi.encodeWithSelector(Errors.PoolConfigurator_InsufficientLiquidity.selector));
+        poolConfigurator.requestFunds({ principal_: 1 });
+    }
+
     function test_requestFunds()
         external
         whenCoverIsSufficient
@@ -85,5 +96,42 @@ contract RequestFunds_Integration_Concrete_Test is PoolConfigurator_Integration_
     function expectPoolConfiguratorPauseRevert() private {
         vm.expectRevert(abi.encodeWithSelector(Errors.PoolConfigurator_Paused.selector));
         poolConfigurator.requestFunds({ principal_: _principal });
+    }
+
+    function _drainThePool() internal {
+        changePrank(users.receiver);
+        pool.requestRedeem(defaults.POOL_SHARES(), users.receiver);
+        vm.warp(defaults.WINDOW_3());
+        pool.redeem(defaults.POOL_SHARES(), users.receiver, users.receiver);
+    }
+
+    /// @dev To create a insufficient liquidity pool
+    /// step 1: Down size the pool, make the pool supply as default principal
+    ///      2: Create loan, the default face amount is same as default principal
+    ///      3: Seller withdraw fund
+    ///      4: Receiver request redeem to increase locked shares
+    function _createInsufficientLiquidityPool() internal {
+        uint256 amount_ = defaults.POOL_SHARES() - _principal;
+
+        // down size the pool
+        changePrank(users.receiver);
+        pool.requestRedeem(amount_, users.receiver);
+        vm.warp(defaults.WINDOW_3());
+        pool.redeem(amount_, users.receiver, users.receiver);
+
+        // create loan
+        createDefaultLoan();
+
+        // seller withdraw fund
+        changePrank(users.seller);
+        IERC721(address(receivable)).approve(address(loanManager), defaults.RECEIVABLE_TOKEN_ID());
+        loanManager.withdrawFunds(1, address(users.seller), _principal);
+
+        // receiver request redeem
+        changePrank(users.receiver);
+        pool.requestRedeem(_principal, users.receiver);
+
+        // move to the next redeemable window
+        vm.warp(defaults.WINDOW_3() + 15 days);
     }
 }
