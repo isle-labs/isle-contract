4959de8cfd06d9c65d180c2417382717dcd0955b
Jung <jwork1994212@gmail.com>
2024-08-02 10:13:08 +0800

    PoolAddressProvider constructor test case supplement

--- tests/unit/concrete/pool-addresses-provider/constructor/constructor.t.sol
+++ tests/unit/concrete/pool-addresses-provider/constructor/constructor.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import { Errors } from "contracts/libraries/Errors.sol";
+
 import { PoolAddressesProvider } from "contracts/PoolAddressesProvider.sol";
 
 import { PoolAddressesProvider_Unit_Shared_Test } from
@@ -9,6 +11,17 @@ import { PoolAddressesProvider_Unit_Shared_Test } from
 contract Constructor_PoolAddressesProvider_Unit_Concrete_Test is PoolAddressesProvider_Unit_Shared_Test {
     function setUp() public virtual override(PoolAddressesProvider_Unit_Shared_Test) {
         PoolAddressesProvider_Unit_Shared_Test.setUp();
+    }
+
+    function test_RevertWhen_InvalidGlobals() external {
+        string memory marketId_ = defaults.MARKET_ID();
+
+        isleGlobals.transferGovernor(address(0));
+
+        vm.expectRevert(
+            abi.encodeWithSelector(Errors.PoolAddressesProvider_InvalidGlobals.selector, address(isleGlobals))
+        );
+        new PoolAddressesProvider(marketId_, isleGlobals);
     }
 
     function test_constructor() external {
