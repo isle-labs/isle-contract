da1a18cce370859a12a59727e605ecd1780870b0
Jung <jwork1994212@gmail.com>
2024-08-05 11:47:08 +0800

    add not enough locked shares case for redeem

--- tests/integration/concrete/pool/redeem/redeem.t.sol
+++ tests/integration/concrete/pool/redeem/redeem.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import { Errors } from "contracts/libraries/Errors.sol";
+
 import { Pool } from "contracts/Pool.sol";
 
 import { Pool_Integration_Shared_Test } from "../../../shared/pool/Pool.t.sol";
@@ -10,7 +12,18 @@ contract Redeem_Pool_Integration_Concrete_Test is Pool_Integration_Shared_Test {
         Pool_Integration_Shared_Test.setUp();
     }
 
-    function test_Redeem() external {
+    modifier whenEnoughLockedShares() {
+        _;
+    }
+
+    function test_RevertWhen_NotEnoughLockedShares() external {
+        uint256 redeemShares_ = defaults.REDEEM_SHARES();
+        vm.expectRevert(abi.encodeWithSelector(Errors.Pool_RedeemMoreThanMax.selector, redeemShares_, 0));
+
+        defaultRedeem();
+    }
+
+    function test_Redeem() external whenEnoughLockedShares {
         requestDefaultRedeem();
 
         vm.warp({ timestamp: defaults.WINDOW_3() });
